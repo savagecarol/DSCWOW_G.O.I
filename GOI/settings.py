f4ad998d656301886b72f21e5a8470571d2261d0
savagecarol <karthiksharma1411@gmail.com>
2020-12-11 22:21:04 +0530

    basic setup done requirements added settings done

--- GOI/settings.py
+++ GOI/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g3zv+hbuj9jugq0bn7+21sd51bo-h6e%#%yyq8gczockqw$a@r'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DEBUG'))
 
 ALLOWED_HOSTS = []
 
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn GOI.wsgi
